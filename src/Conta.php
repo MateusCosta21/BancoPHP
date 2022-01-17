d30a7eeee4a15e86567abad366360af0c61d7d84
João Mateus <jmateuss.costa@gmail.com>
2022-01-17 20:59:49 -0300

    Adicionando função transferência

--- src/Conta.php
+++ src/Conta.php
@@ -3,23 +3,31 @@ class Conta{
     public $cpftitular;
     public $nometitular;
     public $saldo;
-    public function sacar($valoraSacar)
+    public function sacar($valoraSacar) : void
     {
         if($valoraSacar > $this->saldo){
             echo "Você não pode sacar";
+            return;
         }
-        else{
             $this->saldo -= $valoraSacar;
-        }
     }
-    public function depositar($valoraDepositar)
+    public function depositar($valoraDepositar) : void
     {
         if($valoraDepositar <= 0 ){
             echo "Você não pode realizar um deposito";
+            return;
         }
-        else{
             $this->saldo += $valoraDepositar;
+    }
+
+    public function transferencia($valorATransferir, conta $contaDestino) : void
+    {
+        if($valorATransferir > $this -> saldo){
+            echo "saldo indisponivel";
+            return;
         }
+            $this->sacar($valorATransferir);
+            $contaDestino -> depositar($valorATransferir);
     }
 }
 
