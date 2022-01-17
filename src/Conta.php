76ea4584ccecb6f11055039d235c6fcc7cf8ebc0
João Mateus <jmateuss.costa@gmail.com>
2022-01-17 20:37:46 -0300

    Classe com função Depositar e Sacar

--- src/Conta.php
+++ src/Conta.php
@@ -3,18 +3,23 @@ class Conta{
     public $cpftitular;
     public $nometitular;
     public $saldo;
-
-    public function sacar($valoraSacar){
-
+    public function sacar($valoraSacar)
+    {
         if($valoraSacar > $this->saldo){
             echo "Você não pode sacar";
         }
         else{
             $this->saldo -= $valoraSacar;
         }
-
     }
-
-
+    public function depositar($valoraDepositar)
+    {
+        if($valoraDepositar <= 0 ){
+            echo "Você não pode realizar um deposito";
+        }
+        else{
+            $this->saldo += $valoraDepositar;
+        }
+    }
 }
 
